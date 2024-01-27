f51910958f648ca5d4cb312a36c0ddb23ef3e7f0
eduardobattisti <eduardobattisti10@gmail.com>
2024-01-27 20:41:22 -0300

    feat: add ufo plugin to handle folding

--- plugins.lua
+++ plugins.lua
@@ -95,7 +95,7 @@ local plugins = {
       dependencies = "nvim-treesitter/nvim-treesitter",
       event = "BufEnter",
       config = true,
-      version = "*" 
+      version = "*"
   },
   {
     "kylechui/nvim-surround",
@@ -123,6 +123,29 @@ local plugins = {
     lazy = false,
     config = function()
       vim.keymap.set("n", "<leader>gg", vim.cmd.LazyGit)
+    end,
+  },
+  {
+    "kevinhwang91/nvim-ufo",
+    dependencies = "kevinhwang91/promise-async",
+    lazy = false,
+    config = function()
+      local ufo = require('ufo')
+
+      vim.o.foldcolumn = '1' -- '0' is not bad
+      vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+      vim.o.foldlevelstart = 99
+      vim.o.foldenable = true
+
+      -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
+      vim.keymap.set('n', 'zR', ufo.openAllFolds)
+      vim.keymap.set('n', 'zM', ufo.closeAllFolds)
+
+      ufo.setup()
+    end,
+    provider_selector = function(_, filetype, buftype)
+      return (filetype == "" or buftype == "nofile") and "indent" -- only use indent until a file is opened
+        or { "treesitter", "indent" } -- if file opened, try to use treesitter if available
     end,
   },
 }
